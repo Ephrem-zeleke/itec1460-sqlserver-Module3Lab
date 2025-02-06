48d08d189d58c17ec739f09b7a21a5a77b877dd3
Ephrem-zeleke <156279785+Ephrem-zeleke@users.noreply.github.com>
2025-02-06 17:24:58 +0000

    module 4 lab part one

---
+++ Module4Lab.sql
@@ -0,0 +1,26 @@
+--join customers and orders tables because the customer id is a field
+SELECT C.CompanyName, O.OrderDate FROM Customers AS c JOIN Orders AS o ON c.CustomerID = o.CustomerID;
+--query to see all customers (left table ) and the orders table(right table)
+--uses a left join (order date and order id will be null if the customer exists but has no orders)
+SELECT c.CustomerID, c.CompanyName, o.OrderID, o.OrderDate FROM Customers c LEFT JOIN Orders o ON c.CustomerID = o.CustomerID;
+
+--example of using built in function SUM(), ROUND(), and COUNT()
+SELECT OrderID, ROUND(SUM(UnitPrice * Quantity * (1-Discount)),2) AS TotalValue, COUNT(*) AS NumberOfItem FROM [Order Details] GROUP BY OrderID ORDER BY TotalValue DESC /* shows the highst value first*/
+
+-- example of using group by with a having clause
+SELECT p.ProductID, p.ProductName, COUNT(od.OrderID) AS TimesOrdered FROM Products p INNER JOIN [Order Detailes] od ON p.ProductID = od.ProductID GROUP BY p.ProductID, p.ProductName HAVING COUNT(od.OrderID) > 10
+ORDER BY TimesOrdered DESC;
+
+--subquery example
+SELECT ProductName, UnitPrice FROM Products WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products) ORDER BY UnitPrice;
+
+-- this query we limit the result to the top 5 records
+SELECT TOP 5 
+c.CustomerID, c.CompanyName,
+ROUND(SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)), 2) AS TotalPurchase
+FROM Customers c 
+INNER JOIN o ON c.CustomerID = o.CustomerID
+INNER JOIN [Order Detailes] od ON o.OrderID = od.OrderID
+WHERE YEAR(o.OrderDate) = 1997
+GROUP BY c.CustomerID, c.CompanyName
+ORDER BY TotalPurchase DESC;
